55d44977d4b4778d20b23f69fdcb98f3b6b51d10
cdzsbin <zhangshibin@jd.com>
2021-07-09 09:55:12 +0800

    switch dict path

--- src/view/components/dict/DictCheckBox.vue
+++ src/view/components/dict/DictCheckBox.vue
@@ -6,109 +6,109 @@
   </span>
 </template>
 <script>
-import { transferString, toStringArray, transferNumber } from '@/libs/renderUtil.js'
-import constants from '@/constants/constants'
+  import { transferString, toStringArray, transferNumber } from '@/libs/renderUtil.js'
+  import constants from '@/constants/constants'
 
-var _ = require('underscore')
+  var _ = require('underscore')
 
-export default {
-  name: 'DictCheckBox',
-  props: {
-    value: {
-      type: [String, Number, Array, Boolean],
-      default: []
+  export default {
+    name: 'DictCheckBox',
+    props: {
+      value: {
+        type: [String, Number, Array, Boolean],
+        default: function () {
+          return []
+        }
+      },
+      number: {
+        type: Boolean,
+        default: false
+      },
+      kind: {
+        type: String,
+        default: ''
+      },
+      pid: {
+        type: String,
+        default: ''
+      },
+      clearable: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      multiple: {
+        type: Boolean,
+        default: false
+      },
+      transfer: {
+        type: Boolean,
+        default: false
+      },
+      localDictItems: {
+        type: Array,
+        default: null
+      }
     },
-    number: {
-      type: Boolean,
-      default: false
+    data () {
+      return {
+        model: toStringArray(this.value),
+        dictItems: []
+      }
     },
-    kind: {
-      type: String,
-      default: ''
+    mounted () {
+      let _this = this
+      if (this.localDictItems !== null && this.localDictItems.length > 0) {
+        _this.loadDict(this.localDictItems)
+        return
+      }
+      if (this.kind === null || this.kind === '') {
+        return
+      }
+      let key = this.kind
+      if (this.pid !== '') {
+        key = this.kind + '_' + this.pid
+      }
+      if (this.$store !== null && this.$store !== undefined) {
+        let promise = this.$store.dispatch(constants.types.LOAD_DICT_INFO, { kind: key })
+        promise.then((data) => {
+          _this.loadDict(data)
+        })
+      }
     },
-    pid: {
-      type: String,
-      default: ''
-    },
-    clearable: {
-      type: Boolean,
-      default: false
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    multiple: {
-      type: Boolean,
-      default: false
-    },
-    transfer: {
-      type: Boolean,
-      default: false
-    },
-    localDictItems: {
-      type: Array,
-      default: null
-    }
-  },
-  data () {
-    return {
-      model: toStringArray(this.value),
-      dictItems: []
-    }
-  },
-  mounted () {
-    let _this = this
-    if (this.localDictItems !== null && this.localDictItems.length > 0) {
-      _this.loadDict(this.localDictItems)
-      return
-    }
-    if (this.kind === null || this.kind === '') {
-      return
-    }
-    let key = this.kind
-    if (this.pid !== '') {
-      key = this.kind + '_' + this.pid
-    }
-    if (this.$store !== null && this.$store !== undefined) {
-      let promise = this.$store.dispatch(constants.types.LOAD_DICT_INFO, { kind: key })
-      promise.then((data) => {
-        _this.loadDict(data)
-      })
-    }
-  },
-  watch: {
-    value () {
-      let newVal = transferString(this.value)
-      if (_.isArray(newVal)) {
-        if (_.isArray(this.model)) {
-          if (_.difference(newVal, this.model).length !== 0 || this.model.length != newVal.length) {
+    watch: {
+      value () {
+        let newVal = transferString(this.value)
+        if (_.isArray(newVal)) {
+          if (_.isArray(this.model)) {
+            if (newVal.length != this.model.length || _.intersection(newVal, this.model).length !== newVal.length) {
+              this.model = newVal
+            }
+          } else {
             this.model = newVal
           }
         } else {
-          this.model = newVal
-        }
-      } else {
-        if (newVal !== this.model) {
-          this.model = newVal
+          if (newVal !== this.model) {
+            this.model = newVal
+          }
         }
       }
     },
-    localDictItems (newVal, oldVal) {
-      this.loadDict(newVal)
-    }
-  },
-  methods: {
-    updateValue () {
-      let v = this.model
-      if (this.number) {
-        v = transferNumber(v)
+    methods: {
+      updateValue () {
+        let v = this.model
+        if (this.number) {
+          v = transferNumber(v)
+        }
+        this.$emit('input', v)
+        this.$emit('on-change', v)
+      },
+      loadDict (data) {
+        this.dictItems = data
       }
-      this.$emit('input', v)
-    },
-    loadDict (data) {
-      this.dictItems = data
     }
   }
-}
 </script>
